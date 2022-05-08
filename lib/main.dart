66c9fe0f883a160fe91e00d855d92a79e0dc5064
asapbox <tyumenpost@gmail.com>
2022-05-08 14:12:20 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'fooderlich_theme.dart';
+import 'home.dart';
+
+import 'models/models.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.light();
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => TabManager()),
+        ],
+        child: const Home(),
+      ),
+    );
+  }
+}
